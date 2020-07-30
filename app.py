a2673f5fa08643b95553a883c4142087b0d16c2a
Patrick Bucher <patrick.bucher@seantis.ch>
2020-07-30 11:51:31 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import render_template
+
+from data import pupils_grades
+
+app = Flask(__name__)
+
+
+@app.route('/pupils', methods=['GET'])
+def get_pupils():
+    title = 'Notenverwaltung: Schüler'
+    pupils = pupils_grades.keys()
+    return render_template('pupils.jinja2', title=title, pupils=pupils)
+
+
+@app.route('/pupils/<name>', methods=['GET'])
+def get_pupil(name):
+    title = f'Notenverwaltung: {name}'
+    subject_grades = pupils_grades[name]
+    print(subject_grades)
+    return render_template('grades.jinja2', title=title,
+                           subject_grades=subject_grades)
---
+++ data.py
@@ -0,0 +1,12 @@
+pupils_grades = {
+    'Hansruedi': {
+        'Mathematik': [4.9, 3.8, 5.4],
+        'Deutsch': [5.5, 6.0, 4.9],
+        'Englisch': [5.1, 5.0, 4.1],
+    },
+    'Therese': {
+        'Mathematik': [4.2, 5.8, 4.4],
+        'Deutsch': [6.0, 5.9, 5.5],
+        'Englisch': [3.5, 3.9, 4.5],
+    },
+}
