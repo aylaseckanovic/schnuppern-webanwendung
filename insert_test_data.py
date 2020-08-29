aa277bba247d22b86808e541f0f459919d984922
Patrick Bucher <patrick.bucher@seantis.ch>
2020-08-29 16:58:52 +0200

    improved test data: normally distributed grades

--- insert_test_data.py
+++ insert_test_data.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import random
 import sqlite3
+
+import numpy as np
+from scipy.stats import norm
 
 connection = sqlite3.connect('grades.db')
 
@@ -21,10 +24,15 @@ for subject in subjects:
     new_id = c.execute('SELECT last_insert_rowid()')
     subject_ids[subject] = new_id.lastrowid
 
+xs = np.linspace(-3.5, +1.5, 500)
+ys = norm.pdf(xs)
+
 for pupil, pupil_id in pupil_ids.items():
+    baseline = 4.5 + random.choices(xs, weights=ys, k=1)[0]
     for subject, subject_id in subject_ids.items():
-        for i in range(0, random.randint(1, 10)):
-            grade = random.randint(10, 60) / 10
+        deviations = random.choices(xs, weights=ys, k=random.randint(1, 10))
+        for deviation in deviations:
+            grade = round((baseline + deviation) * 10) / 10
             c.execute('INSERT INTO grade (pupil_id, subject_id, grade) '
                       f'VALUES ({pupil_id}, {subject_id}, {grade})')
 
